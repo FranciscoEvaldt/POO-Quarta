aa8e4de1201210ebcb68ccbb917b27a95c7e961a
Francisco Evaldt <102334912+FranciscoEvaldt@users.noreply.github.com>
2023-03-29 14:35:06 -0300

    Add files via upload

---
+++ Aula_02/Program.cs
@@ -0,0 +1,29 @@
+﻿using Aula_02;
+
+Livro livroBD = new Livro(2323,"e939393939", "Banco de dados filosófico",240);
+
+
+livroBD.Preco = 290;
+Show(livroBD.Id + "-" + livroBD.Isbn + " - " + livroBD.Preco);
+
+Show("digite algo:");
+var retorno = Console.ReadLine();
+
+Show("o usuário retornou " + retorno);
+
+Show("digite um número: ");
+double num1 = Convert.ToDouble( Console.ReadLine());
+
+Show("digite mais um número: ");
+double num2 = Convert.ToDouble( Console.ReadLine());
+
+Calculadora calc = new Calculadora();
+var result = calc.Calcular(num1,num2,"+");
+
+Show("a soma é " + result);
+
+
+void Show(string msg)
+{
+    Console.WriteLine(msg);
+}
