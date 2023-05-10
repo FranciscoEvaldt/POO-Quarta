544e0fecb82fee21ac2d5b24b8b2324c0a9a90e3
Francisco Evaldt <102334912+FranciscoEvaldt@users.noreply.github.com>
2023-05-10 19:16:14 -0300

    Add files via upload

---
+++ Aula_12/aula12_ef_test/Data/DataContext.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using aula12_ef_test.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace aula12_ef_test.csproj
+{
+    public class DataContext : DbContext
+    {
+       public string DbPath { get; }
+
+        public DataContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "TestEf.db");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Aula_12/aula12_ef_test/Program.cs
@@ -0,0 +1,4 @@
+﻿using aula12_ef_test.csproj;
+
+var db = new DataContext();
+Console.WriteLine($"Database path: {db.DbPath}.");
