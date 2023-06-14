1f5d3f6491735ed284b14a713cd1a8639e569aeb
Francisco Evaldt <102334912+FranciscoEvaldt@users.noreply.github.com>
2023-06-14 12:07:55 -0300

    Delete ap2_crud_medicos/AP2-POO-main directory

--- ap2_crud_medicos/AP2-POO-main/Domain/Entities/Paciente.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ap2_crud_medicos.Domain.Entities
-{
-  public class Paciente : Pessoa
-  {
-    public List<Consulta> Consultas { get; set; } = new List<Consulta>();
-  }
-}
